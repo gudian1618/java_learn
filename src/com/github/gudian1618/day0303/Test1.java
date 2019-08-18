65ed76d84daff53ec83aa276e4ae9dae0e3f5d70
gudian1618 <1761844211@qq.com>
2019-08-18 15:05:35 +0800

    新建

---
+++ src/com/github/gudian1618/day0303/Test1.java
@@ -0,0 +1,26 @@
+package com.github.gudian1618.day0303;
+
+import java.util.Scanner;
+
+/**
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019-08-18 15:03
+ */
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("输入整数，求阶乘：");
+        int n = new Scanner(System.in).nextInt();
+        long r = f(n);
+        System.out.println(r);
+    }
+
+    private static long f(int n) {
+
+
+    }
+}
+
+
