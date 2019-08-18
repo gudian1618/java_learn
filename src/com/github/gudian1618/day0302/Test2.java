0e098cb475e908e6a0588997b14bf5d04223163a
gudian1618 <1761844211@qq.com>
2019-08-18 11:51:48 +0800

    新建

--- src/com/github/gudian1618/day0302/Test2.java
+++ src/com/github/gudian1618/day0302/Test2.java
@@ -1,6 +1,7 @@
 package com.github.gudian1618.day0302;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * @author gudian1618
@@ -30,18 +31,8 @@ public class Test2 {
         int[] red = xuanRed(r);
 //        选择篮球
         int blue = xuanBlue(b);
-        System.out.println("红球：" + Arrays.toString(r));
-        System.out.println("蓝球：" + b);
-
-    }
-
-    private static int xuanBlue(int[] b) {
-
-
-    }
-
-    private static int[] xuanRed(int[] r) {
-
+        System.out.println("红球：" + Arrays.toString(red));
+        System.out.println("蓝球：" + blue);
 
     }
 
@@ -53,6 +44,30 @@ public class Test2 {
         }
         return a;
 //        把数组的地址值返回到调用位置
+    }
+
+    private static int[] xuanRed(int[] r) {
+        /*
+         * 选择6个不重复的红球
+         * i变量，从0到<6，访问前6个位置
+         * 随机定位下标j，范围i+[0,33-i)
+         * i和j位置的值进行交换
+         * 截取前六个位置复制新的数组
+         * 返回新数组
+         * */
+        for (int i = 0; i < 6; i++) {
+//            随机定位j，范围i+[0,33-i)
+            int j = i + new Random().nextInt(33 - i);
+//            i,j交换
+            int t = r[i];
+            r[i] = r[j];
+            r[j] = t;
+        }
+        return Arrays.copyOf(r, 6);
+    }
+
+    private static int xuanBlue(int[] b) {
+        return b[new Random().nextInt(16)];
     }
 }
 
