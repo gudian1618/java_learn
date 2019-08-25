422925b5d1c9fc6214a7943c21e6ea381d412fd5
gudian1618 <1761844211@qq.com>
2019-08-25 16:02:33 +0800

    新建

--- src/com/github/gudian1618/day0404/Shape.java
+++ src/com/github/gudian1618/day0404/Shape.java
@@ -4,15 +4,16 @@ package com.github.gudian1618.day0404;
  * @author gudian1618
  * @version 1.0
  * @date 2019/8/25 3:12 下午
- * 定义父类“形状”
+ * 定义父类“形状”，并且定义一个抽象父类
  */
 
-public class Shape {
+public abstract class Shape {
 
-    public void deaw() {
-        // 无意义代码，需要在子类中重写
-        System.out.println("图形形状");
-    }
+    public abstract void deaw();
+//    {
+//        // 无意义代码，需要在子类中重写
+//        System.out.println("图形形状");
+//    }
 
     public void clear() {
         System.out.println("\n\n");
