6396ebac5d714c870dd4280860c79f2f4c49972e
gudian1618 <1761844211@qq.com>
2019-08-25 15:12:54 +0800

    新建

---
+++ src/com/github/gudian1618/day0404/Line.java
@@ -0,0 +1,10 @@
+package com.github.gudian1618.day0404;
+
+/**
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019/8/25 3:12 下午
+ */
+
+public class Line {
+}
---
+++ src/com/github/gudian1618/day0404/Test1.java
@@ -0,0 +1,15 @@
+package com.github.gudian1618.day0404;
+
+/**
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019/8/25 3:12 下午
+ */
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
