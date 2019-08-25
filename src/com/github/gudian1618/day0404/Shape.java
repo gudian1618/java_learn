df86c084461f6786dc388492dee4155ede2fdd24
gudian1618 <1761844211@qq.com>
2019-08-25 15:16:47 +0800

    新建

--- src/com/github/gudian1618/day0404/Shape.java
+++ src/com/github/gudian1618/day0404/Shape.java
@@ -7,5 +7,12 @@ package com.github.gudian1618.day0404;
  */
 
 public class Shape {
+    public void deaw() {
+        // 无意义代码，需要在子类中重写
+        System.out.println("图形形状");
+    }
 
+    public void clear() {
+        System.out.println("\n\n\n\n\\n\n\n\n\n\n\n\\n\n");
+    }
 }
