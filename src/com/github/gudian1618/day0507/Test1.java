85a175c95b95ff46ec1d3d5b701214ac00b2bd76
gudian1618 <1761844211@qq.com>
2019-09-01 15:29:06 +0800

    新建

---
+++ src/com/github/gudian1618/day0507/Test1.java
@@ -0,0 +1,15 @@
+package com.github.gudian1618.day0507;
+
+/**
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019/9/1 3:28 下午
+ */
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
