ec8ddec3baefe5c19c69ac1716ca387c3dd08cd5
gudian1618 <1761844211@qq.com>
2019-09-08 11:19:37 +0800

    新建

--- src/com/github/gudian1618/day0602/Test1.java
+++ src/com/github/gudian1618/day0602/Test1.java
@@ -7,4 +7,18 @@ package com.github.gudian1618.day0602;
  */
 
 public class Test1 {
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        // s.id = 6;是私有变量，不能直接访问
+        s.setId(6);
+        s.setName("张三");
+        s.setGender("男");
+        s.setAge(12);
+
+        System.out.println(s.getId());
+        System.out.println(s.getName());
+        System.out.println(s.getGender());
+        System.out.println(s.getAge());
+    }
 }
